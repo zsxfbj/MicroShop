d119f4b8f7e732f694e56cb3b8a7c9651376c456
盛雄峰 <sxfbuct@msn.com>
2021-11-03 01:55:39 +0800

    [add] 添加相关中间类

---
+++ MicroShop.Model.Product/BrandDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MicroShop.Model.Product
+{
+    /// <summary>
+    /// 品牌视图
+    /// </summary>
+    [Serializable]
+    public class BrandDTO
+    {
+        /// <summary>
+        /// 品牌编号
+        /// </summary>
+        [Description("品牌编号，自增编号")]
+        [DisplayName("品牌编号")]
+        public Int32 BrandId { get; set; }
+
+        /// <summary>
+        /// 品牌名称
+        /// </summary>
+        [Description("品牌名称，最多30个字")]
+        [DisplayName("品牌名称")]
+        public String BrandName { get; set; }
+    }
+}
---
+++ MicroShop.Model.Product/ProductTypeEnum.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace MicroShop.Model.Product
+{
+    /// <summary>
+    /// 产品类型枚举
+    /// </summary>
+    public enum ProductTypeEnum
+    {
+        /// <summary>
+        /// 商品
+        /// </summary>
+        [Description("商品")]
+        [EnumMember(Value = "1")]
+        Item = 1,
+
+        /// <summary>
+        /// 电子卡
+        /// </summary>
+        [Description("电子卡")]
+        [EnumMember(Value = "2")]
+        Card = 2,
+
+        /// <summary>
+        /// 服务
+        /// </summary>
+        [Description("服务")]
+        [EnumMember(Value = "3")]
+        Service = 3
+    }
+}
