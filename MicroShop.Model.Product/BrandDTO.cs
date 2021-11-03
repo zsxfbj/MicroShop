2c230cb24f35aadb76587d8fd388d819a51f4005
盛雄峰 <sxfbuct@msn.com>
2021-11-03 10:00:24 +0800

    【modify】修改品牌数据视图

--- MicroShop.Model.Product/BrandDTO.cs
+++ MicroShop.Model.Product/BrandDTO.cs
@@ -22,5 +22,34 @@ namespace MicroShop.Model.Product
         [Description("品牌名称，最多30个字")]
         [DisplayName("品牌名称")]
         public String BrandName { get; set; }
+
+        /// <summary>
+        /// 品牌英文名
+        /// </summary>
+        [Description("品牌英文或者拼音名称，最多64个字")]
+        [DisplayName("品牌英文名")]
+        public String BrandEnglishName { get; set; }
+
+        /// <summary>
+        /// 品牌Logo
+        /// </summary>
+        [Description("品牌Logo图片地址，最多256个字")]
+        [DisplayName("品牌Logo")]
+        public String LogoUrl { get; set; }
+
+        /// <summary>
+        /// 是否推荐
+        /// </summary>
+        [Description("是否推荐，true：是；false：否")]
+        [DisplayName("是否推荐")]
+        public Boolean IsRecommend { get; set; }
+
+        /// <summary>
+        /// 前缀
+        /// </summary>
+        [Description("前缀，品牌英文单词（或者拼音）首字母")]
+        [DisplayName("前缀")]
+        public String PreFix { get; set; }
+
     }
 }
