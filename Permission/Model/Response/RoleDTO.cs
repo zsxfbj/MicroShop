7de408e8f3eb67f1def35a367c26bef31266716a
Peak Sheng <sxfbuct@msn.com>
2022-02-11 14:40:05 +0800

    add web api and web common

---
+++ Permission/Model/Response/RoleDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MicroShop.Permission.Model.Response
+{
+    /// <summary>
+    /// 角色数据视图
+    /// </summary>
+    [Serializable]
+    public class RoleDTO
+    {
+        /// <summary>
+        /// 角色编号
+        /// </summary>
+        [Description("角色编号")]
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        [Description("角色名称")]
+        public string RoleName { get; set; }
+
+
+
+    }
+}
---
+++ Web/Common/User/UserTokenDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MicroShop.Web.Common.User
+{
+    /// <summary>
+    /// 用户访问令牌类，存储相关的信息
+    /// </summary>
+    [Serializable]
+    public class UserTokenDTO
+    {
+        /// <summary>
+        /// 令牌
+        /// </summary>
+        [Key]
+        [Required(ErrorMessage = "访问令牌不能为空")]
+        public string Token { get; set; }
+
+        /// <summary>
+        /// 用户编号
+        /// </summary>
+        
+        public long UserId { get; set; }
+
+
+
+
+    }
+}
