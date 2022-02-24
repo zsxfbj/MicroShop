8798441e69f3890e66614a219a96993995ce39bd
Peak Sheng <sxfbuct@msn.com>
2022-02-24 17:04:15 +0800

    add role and menu relation business logic method

---
+++ Permission/BLL/BRoleMenuRelation.cs
@@ -0,0 +1,53 @@
+﻿using MicroShop.Permission.Entity;
+using MicroShop.Permission.Model.Request;
+using MicroShop.Web.Common;
+
+namespace MicroShop.Permission.BLL
+{
+    /// <summary>
+    /// 角色菜单关系
+    /// </summary>
+    public class BRoleMenuRelation : Utility.Common.Singleton<BRoleMenuRelation>
+    {
+        #region public void SetRoleMenuRelation(RoleMenuRelationDTO roleMenuRelation, PermissionContext context)
+        /// <summary>
+        /// 设置
+        /// </summary>
+        /// <param name="roleMenuRelation"></param>
+        public void SetRoleMenuRelation(RoleMenuRelationDTO roleMenuRelation, PermissionContext context)
+        {
+            if(roleMenuRelation == null)
+            {
+                throw new ServiceException { ErrorCode = RequestResultCodeEnum.RequestParameterError, ErrorMessage = "请求参数不能为空" };
+            }
+
+            if(roleMenuRelation.RoleId < 1)
+            {
+                throw new ServiceException { ErrorCode = RequestResultCodeEnum.RequestParameterError, ErrorMessage = "角色编号格式错误" };
+            }
+
+            if(context == null)
+            {
+                context = new PermissionContext();
+            }
+
+            List<RoleMenuRelationEntity> roleMenuRelations = context.RoleMenuRelations.Where(x => x.RoleId == roleMenuRelation.RoleId).ToList();
+            if (roleMenuRelations.Count > 0)
+            {
+                context.RoleMenuRelations.RemoveRange(roleMenuRelations);
+                context.SaveChanges();
+            }
+
+            if (roleMenuRelation.MenuIds != null && roleMenuRelation.MenuIds.Count(x=>x> 0) > 0)
+            {
+                roleMenuRelations = roleMenuRelation.MenuIds.Select(x => new RoleMenuRelationEntity { MenuId = x, CreatedAt = DateTime.Now, RoleId = roleMenuRelation.RoleId }).ToList();
+                context.RoleMenuRelations.AddRange(roleMenuRelations);
+                context.SaveChanges();
+            }             
+        }
+        #endregion public void SetRoleMenuRelation(RoleMenuRelationDTO roleMenuRelation, PermissionContext context)
+
+
+       
+    }
+}
