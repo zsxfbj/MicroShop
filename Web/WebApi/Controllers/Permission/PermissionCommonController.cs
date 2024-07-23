dbf537636ab8fb29baa72ccd92882bd95593d124
盛雄峰 <zsxfbj@gmail.com>
2024-07-23 16:17:08 +0800

    修改验证码图片输出的格式

--- Web/WebApi/Controllers/Permission/PermissionCommonController.cs
+++ Web/WebApi/Controllers/Permission/PermissionCommonController.cs
@@ -32,7 +32,7 @@ namespace MicroShop.Web.AdminApi.Controllers.Permission
             string accessToken = Utility.Common.HttpContext.GetHeaderValue(HeaderParameters.SYSTEM_USER_AUTH_TOKEN_KEY);
             CaptchaData info = _captcha.Generate(accessToken);
             Utility.Common.HttpContext.Current.Response.Headers.Add(HeaderParameters.SYSTEM_USER_AUTH_TOKEN_KEY, accessToken);
-            return new FileContentResult(info.Bytes, "image/gif");
+            return new FileContentResult(info.Bytes, "image/png");
         }
     }
 }
