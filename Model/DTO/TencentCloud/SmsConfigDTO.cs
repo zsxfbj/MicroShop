82a7d0e528d83c605b920b16a923d87c1b543ca1
Peak Sheng <zsxfbj@gmail.com>
2024-08-05 13:21:50 +0800

    调整项目类型及结构

---
+++ Model/DTO/TencentCloud/SmsConfigDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MicroShop.Model.DTO.TencentCloud
+{
+    /// <summary>
+    /// 腾讯云短信通道参数
+    /// </summary>
+    [Serializable]
+    public class SmsConfigDTO
+    {
+        /// <summary>
+        /// 信应用的唯一标识
+        /// </summary>
+        public string AppId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 校验短信发送合法性的密码
+        /// </summary>
+        public string AppKey { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 短信签名
+        /// </summary>
+        public string Sign {  get; set; } = string.Empty;
+
+        /// <summary>
+        /// 访问API接口的AppId
+        /// </summary>
+        public string ApiAppId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 访问API接口的密钥Id
+        /// </summary>
+        public string SecretId { get; set; } = string.Empty;
+        /// <summary>
+        /// 访问API接口的密钥
+        /// </summary>
+        public string SecretKey { get; set; } = string.Empty;
+        
+    }
+}
--- Utility/Cache/MemcacheClient.cs
+++ Utility/Cache/MemcacheClient.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.Caching;
 using System.Text.Json;
 using MicroShop.Utility.Common;
