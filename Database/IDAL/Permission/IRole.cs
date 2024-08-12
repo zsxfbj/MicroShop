db4b5382abc361c02d60215e15158cdf92b45985
zsxfbj <zsxfbj@gmail.com>
2024-08-12 15:43:41 +0800

    add basebase idal module

---
+++ Database/IDAL/Permission/IRole.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using MicroShop.Common.Model.DTO.Permission;
+using MicroShop.Common.Model.VO.Permission;
+using MicroShop.Common.Model.VO.Web;
+
+namespace MicroShop.Database.IDAL.Permission
+{
+    /// <summary>
+    /// Role数据访问接口
+    /// </summary>
+    public interface IRole
+    {
+        RoleVO GetRole(long roleId);
+
+        RoleVO? GetRole(string roleName);
+
+        RoleVO Create(CreateRoleReqDTO req);
+
+        RoleVO Modify(ModifyRoleReqDTO req);
+ 
+        void Delete(long roleId);
+
+        PageResultVO<RoleVO> GetPageResult(RolePageReqDTO req);
+
+        List<RoleVO> GetRoles();
+    }
+}
---
+++ Database/IDAL/Permission/IRoleMenuRelation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using MicroShop.Common.Model.DTO.Permission;
+using MicroShop.Common.Model.VO.Permission;
+
+namespace MicroShop.Database.IDAL.Permission
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IRoleMenuRelation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <param name="menuId"></param>
+        /// <returns></returns>
+        bool HasPermission(long roleId, string permission);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="roleMenuRelation"></param>
+        void SetRoleMenuRelation(SetRoleMenuRelationReqDTO roleMenuRelation);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        List<RoleMenuVO> GetRoleMenus(long roleId, bool showHidden);
+    }
+}
